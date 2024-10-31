21efc85e563761703d960edd5d7f5323cd3dfbf3
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-10-31 22:15:39 +0300

    Add footer

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,11 +1,19 @@
 import Header from "@/components/header";
+import Footer from "@/components/footer";
+import Card from "react-bootstrap/Card";
+import { Container } from "react-bootstrap";
 
 function Main(): JSX.Element {
   return (
-    <>
+    <div className="d-flex flex-column vh-100">
       <Header />
-      <div>Сайт программы веб-технологий</div>
-    </>
+      <Container className="d-flex flex-column h-100 py-5 gap-3">
+        <Card className="p-3">
+          <Card.Text>Сайт программы веб-технологий</Card.Text>
+        </Card>
+      </Container>
+      <Footer />
+    </div>
   );
 }
 
