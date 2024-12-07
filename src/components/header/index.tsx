ed988fbc30c7a5008dcdbc1733db7cb5d2bf4518
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-12-07 17:22:39 +0300

    Set file paths as constants

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,12 @@
 import { Navbar, Container } from "react-bootstrap";
 import ThemeSwitcher from "@/components/theme-switcher";
+import { APP_BASE_PATH } from "@/constants";
 
 function Header(): JSX.Element {
   return (
     <Navbar bg="primary">
       <Container>
-        <Navbar.Brand
-          href={`${import.meta.env.BASE_URL}`}
-          style={{ color: "white" }}
-        >
+        <Navbar.Brand href={APP_BASE_PATH} style={{ color: "white" }}>
           <img
             alt="ИТМО"
             src="images/itmo-web-tech-logo-light.svg"
